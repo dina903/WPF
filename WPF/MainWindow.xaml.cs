d8e7dbb309c2277e8af0dc4fdcf57e1334c49fde
Dina Najeeb <dina.najeeb.903@my.csun.edu>
2014-10-30 18:57:44 -0700

    onclick attempts

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security;
+using System.Collections;
+using System.Diagnostics;
 
 namespace WPF
 {
     public partial class MainWindow : Window
     {
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
-           
         }
-        
+        // The selected points.
+        private List<Point> Corners = new List<Point>();
+        private Point LastPoint;
+        private int RADIUS = 3;
+
+        void Canvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+ 
+            Console.WriteLine("Canvas click");
+            e.Handled = true;
+        }
+
     }
 }
