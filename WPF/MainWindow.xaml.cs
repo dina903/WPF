c6343979d52c3a3ebc6e8122f25c40d7abf18413
karoon80 <karoon.gayzagian.621@my.csun.edu>
2014-11-02 13:07:56 -0800

    Implemented the graph, added Clear button code.

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -15,11 +15,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Security;
 using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.ComponentModel;
-
+using System.Timers;
 
 namespace WPF
 {
@@ -54,21 +53,89 @@ namespace WPF
         //Right Mouse Button Up Event handler
         private void main_canvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
-          //  MessageBox.Show("releaseEllispse: \r\t sender = " + sender.ToString() + " \r\t source = " + e.Source.ToString() + " \r\t original source = " + e.OriginalSource.ToString());
             // Save the point.
             cp.Add(new Point(e.GetPosition(main_canvas).X, e.GetPosition(main_canvas).Y));
 
-// Draw the new point.
-/*            using (Graphics gr = this.CreateGraphics())
-            { 
-              if (cp.Count == 1) gr.Clear(this.Background);
-              gr.DrawEllipse(Pens.Blue,
-                  e.GetPosition(main_canvas).X - radius, e.GetPosition(main_canvas).Y - radius,
-                  2 * radius, 2 * radius);
-            }    */
-            
+            // Draw the new point.
+            if (cp.Count == 1) 
+                main_canvas.Children.Clear();
+            Ellipse controlPoint= new Ellipse();
+            controlPoint.Stroke = new SolidColorBrush(Colors.Black);
+            controlPoint.StrokeThickness = 3;
+            controlPoint.Width = 2 * radius;
+            controlPoint.Height = 2 * radius;
+            Canvas.SetLeft(controlPoint, e.GetPosition(main_canvas).X-radius);
+            Canvas.SetTop(controlPoint, e.GetPosition(main_canvas).Y-radius);
+            main_canvas.Children.Add(controlPoint);   
+        }
+        //Run Button onClick code
+       private void runBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Start running.
+            if (cp.Count < 3)
+            {
+                // We need more points.
+               var result =  MessageBox.Show(this,"At least THREE points before clicking Run","Need More Points", MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+            }
+            else if(cp.Count > 6)
+            {
+                var result = MessageBox.Show(this, "At most SIX points before clicking Run. Start Over", "Clear The Canvas", MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                // Draw the corners.
+                foreach (Point pt in cp)
+                {
+                    Ellipse whiteSpace = new Ellipse();
+                    whiteSpace.Width = 2 * radius;
+                    whiteSpace.Height = 2 * radius;
+                    whiteSpace.Fill = new SolidColorBrush(Colors.White);
+                    Canvas.SetLeft(whiteSpace, pt.X-radius);
+                    Canvas.SetTop(whiteSpace, pt.Y-radius);
+                    main_canvas.Children.Add(whiteSpace);
+
+                    Ellipse ellipse = new Ellipse();
+                    ellipse.Width = 2 * radius;
+                    ellipse.Height = 2 * radius;
+                    ellipse.Stroke = new SolidColorBrush(Colors.Blue);
+                    ellipse.StrokeThickness = 1;
+                    //whiteSpace.Fill = new SolidColorBrush(Colors.White);
+                    Canvas.SetLeft(ellipse, pt.X - radius);
+                    Canvas.SetTop(ellipse, pt.Y - radius);
+                    main_canvas.Children.Add(ellipse);
+                }
+
+                // Draw points.
+                Random rand = new Random();
+                last = cp[rand.Next(0, cp.Count)];
+                // Draw 2000 points.
+                for (int i = 1; i <= 2000; i++)
+                {
+                    int j = rand.Next(0, cp.Count);
+                    last = new Point(
+                        (last.X + cp[j].X) / 2,
+                        (last.Y + cp[j].Y) / 2);
+                    Line ellipseRepeat = new Line();
+                    ellipseRepeat.X1 = last.X;
+                    ellipseRepeat.Y1 = last.Y;
+                    ellipseRepeat.X2 = last.X + 1;
+                    ellipseRepeat.Y2 = last.Y + 1;
+                    ellipseRepeat.Stroke = new SolidColorBrush(Colors.Blue);
+                    ellipseRepeat.StrokeThickness = 1;
+                    //whiteSpace.Fill = new SolidColorBrush(Colors.White);
+                   
+                    main_canvas.Children.Add(ellipseRepeat);
+                   
+                }
+            } 
         }
 
-       
+       private void clearBtn_Click(object sender, RoutedEventArgs e)
+       {
+           main_canvas.Children.Clear();
+           cp.Clear();
+       }
     }
 }
