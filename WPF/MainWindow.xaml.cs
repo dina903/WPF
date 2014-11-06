91e5bed8dfe51985cd3f278e70f02337a37575c5
Dina Najeeb <dina.najeeb.903@my.csun.edu>
2014-11-05 17:58:20 -0800

    experminting with line

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -29,12 +29,12 @@ namespace WPF
         {
             InitializeComponent();
         }
-        // The selected points.
-        private List<Point> cp = new List<Point>();
+        //The selected points.
+        //private List<Point> cp = new List<Point>();
         private List<myCustomPoint> customeList = new List<myCustomPoint>();
 
         private myCustomPoint myP;
-        private Point last;
+        private myCustomPoint last;
         private int radius = 2;
         private Color myColor = Colors.Blue;
 
@@ -59,11 +59,10 @@ namespace WPF
         private void main_canvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
             // Save the point.
-            cp.Add(new Point(e.GetPosition(main_canvas).X, e.GetPosition(main_canvas).Y));
+            //cp.Add(new Point(e.GetPosition(main_canvas).X, e.GetPosition(main_canvas).Y));
             radius = checkSize(); // check combobox for current value
             // Draw the new point.
-            if (cp.Count == 1)
-                main_canvas.Children.Clear();
+            
             Ellipse controlPoint = new Ellipse();
             checkColor();
             controlPoint.Fill = new SolidColorBrush(myColor);
@@ -72,6 +71,9 @@ namespace WPF
 
             myP = new myCustomPoint(new Point(e.GetPosition(main_canvas).X, e.GetPosition(main_canvas).Y), myColor, radius);
             customeList.Add(myP);
+
+            if (customeList.Count == 1)
+                main_canvas.Children.Clear();
 
             Canvas.SetLeft(controlPoint, e.GetPosition(main_canvas).X - radius);
             Canvas.SetTop(controlPoint, e.GetPosition(main_canvas).Y - radius);
@@ -115,13 +117,13 @@ namespace WPF
         private void runBtn_Click(object sender, RoutedEventArgs e)
         {
             // Start running.
-            if (cp.Count < 3)
+            if (customeList.Count < 3)
             {
                 // We need more points.
                 var result = MessageBox.Show(this, "At least THREE points before clicking Run", "Need More Points", MessageBoxButton.OK,
                      MessageBoxImage.Exclamation);
             }
-            else if (cp.Count > 6)
+            else if (customeList.Count > 6)
             {
                 var result = MessageBox.Show(this, "At most SIX points before clicking Run. Start Over", "Clear The Canvas", MessageBoxButton.OK,
                     MessageBoxImage.Exclamation);
@@ -129,39 +131,51 @@ namespace WPF
             else
             {
                 // Draw the corners.
-                foreach (Point pt in cp)
+                foreach (myCustomPoint pt in customeList)
                 {
                     Rectangle sqr = new Rectangle();
                     sqr.Width = 2 * radius;
                     sqr.Height = 2 * radius;
-                    sqr.Fill = new SolidColorBrush(Colors.Black);
 
-                    Canvas.SetLeft(sqr, pt.X - 2 * radius);
-                    Canvas.SetTop(sqr, pt.Y - 2 * radius);
+                    sqr.Fill = new SolidColorBrush(pt.pColor);
+
+                    Canvas.SetLeft(sqr, pt.coordinate.X - 2 * radius);
+                    Canvas.SetTop(sqr, pt.coordinate.Y - 2 * radius);
                     main_canvas.Children.Add(sqr);
                 }
 
                 // Draw points.
                 Random rand = new Random();
-                last = cp[rand.Next(0, cp.Count)];
+                last = customeList[rand.Next(0, customeList.Count)];
                 // Draw 2000 points.
                 for (int i = 1; i <= 2000; i++)
                 {
-                    int j = rand.Next(0, cp.Count);
-                    last = new Point((last.X + cp[j].X) / 2, (last.Y + cp[j].Y) / 2);
-
-                    Ellipse myElli = new Ellipse();
+                    int j = rand.Next(0, customeList.Count);
                     Color parentColor = customeList[j].pColor;
+                    int parentRadius = customeList[j].pRadius;
+
+                    last = new myCustomPoint(new Point((last.coordinate.X + customeList[j].coordinate.X) / 2, (last.coordinate.Y + customeList[j].coordinate.Y) / 2), parentColor, parentRadius);
+
+                    Line repeatedLine = new Line();
+                    repeatedLine.X1 = last.coordinate.X;
+                    repeatedLine.Y1 = last.coordinate.Y;
+                    repeatedLine.X2 = last.coordinate.X + 1;
+                    repeatedLine.Y2 = last.coordinate.Y + 1;
+                    
+                    repeatedLine.Stroke = new SolidColorBrush(parentColor);
+                    repeatedLine.StrokeThickness = parentRadius;
+                    main_canvas.Children.Add(repeatedLine);
+
+                    /*Ellipse myElli = new Ellipse();
 
                     myElli.Fill = new SolidColorBrush(parentColor);
                     myElli.Width = 2 * customeList[j].pRadius;
                     myElli.Height = 2 * customeList[j].pRadius;
 
-                    Canvas.SetLeft(myElli, last.X - customeList[j].pRadius);
-                    Canvas.SetTop(myElli, last.Y - customeList[j].pRadius);
+                    Canvas.SetLeft(myElli, last.coordinate.X - parentRadius);
+                    Canvas.SetTop(myElli, last.coordinate.Y - parentRadius);
 
-                    main_canvas.Children.Add(myElli);
-
+                    main_canvas.Children.Add(myElli);*/
                 }
 
             }
@@ -172,7 +186,7 @@ namespace WPF
         {
             main_canvas.Children.Clear();
             customeList.Clear();
-            cp.Clear();
+            //cp.Clear();
         }
     }
 }
